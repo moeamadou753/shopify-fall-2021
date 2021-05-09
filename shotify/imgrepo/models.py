97fff2a2fe291d96c3000c7dc41a307fc2239dd0
GitHub <mohamed.amadou159@gmail.com>
2021-05-09 15:53:58 -0400

    create basic image model and manager

--- shotify/imgrepo/models.py
+++ shotify/imgrepo/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class ImageManager(models.Manager):
+    def create_image(self, owner, title, slug, permissions, src, pub_date):
+        image = self.create(owner=owner, title=title, slug=slug, permissions=permissions, src=src, pub_date=pub_date)
+        # do something with the book
+        return image
+
+class Image(models.Model):
+    image_owner = models.CharField(max_length=30)
+    image_title = models.CharField(max_length=50)
+    image_slug = models.SlugField(max_length=200)
+    image_permissions = models.CharField(max_length=1, choices=[('+', 'PUBLIC'), ('-', 'PRIVATE')], default='+')
+    image_src = models.FileField(upload_to='/images/')
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    objects = ImageManager()
