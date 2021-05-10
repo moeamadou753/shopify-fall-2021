ef011945d715722a17729ccee91ac4abd4e9327a
GitHub <mohamed.amadou159@gmail.com>
2021-05-09 22:03:12 -0400

    add basic authentication system

--- shotify/shotify/urls.py
+++ shotify/shotify/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('repo/', include('imgrepo.urls')),
+    path('', include('imgrepo.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
