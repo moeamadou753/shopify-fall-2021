6b83568f098af7bb176cd5ac9d8722c15099aca9
GitHub <mohamed.amadou159@gmail.com>
2021-05-09 21:44:37 -0400

    remove extra subdomain

--- shotify/imgrepo/urls.py
+++ shotify/imgrepo/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     # i.e. images/moe/
-    path('images/<str:username>/', views.user_images, name='user_images'),
+    path('<str:username>/', views.user_images, name='user_images'),
     # i.e. images/moe/3
     # TODO: change to slug
-    path('images/<str:username>/<int:image_id>/', views.detail, name='detail')
+    path('<str:username>/<int:image_id>/', views.detail, name='detail')
 ]
