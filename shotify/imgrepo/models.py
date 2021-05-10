5b3c43dcb1ad8d52c346da358a680464b9095795
GitHub <mohamed.amadou159@gmail.com>
2021-05-09 20:45:11 -0400

    add model to string

--- shotify/imgrepo/models.py
+++ shotify/imgrepo/models.py
@@ -11,7 +11,10 @@ class Image(models.Model):
     image_title = models.CharField(max_length=50)
     image_slug = models.SlugField(max_length=200)
     image_permissions = models.CharField(max_length=1, choices=[('+', 'PUBLIC'), ('-', 'PRIVATE')], default='+')
-    image_src = models.FileField(upload_to='/images/')
+    image_src = models.FileField(upload_to='images/')
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     objects = ImageManager()
+
+    def __str__(self):
+        return f"{self.image_title} by {self.image_owner} on {self.pub_date}"
