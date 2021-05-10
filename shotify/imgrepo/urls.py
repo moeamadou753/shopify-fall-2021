f241a7d4d19aa5d0807981557cb81123857d8192
GitHub <mohamed.amadou159@gmail.com>
2021-05-10 01:31:53 -0400

    add media root and url

--- shotify/imgrepo/urls.py
+++ shotify/imgrepo/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,3 +12,6 @@ urlpatterns = [
     # TODO: change to slug
     path('<str:username>/<int:image_id>/', views.detail, name='detail')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
